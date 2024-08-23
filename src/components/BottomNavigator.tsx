e7ae165df8a2249e61ac21aded9a87e16c40c437
Supragya2912 <anandsupragya@gmail.com>
2024-08-24 00:20:45 +0530

    added routes on bottom navigation

--- src/components/BottomNavigator.tsx
+++ src/components/BottomNavigator.tsx
@@ -1,41 +1,58 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
+import SearchScreen from '../screens/SearchScreen';
 import Icon from 'react-native-vector-icons/Ionicons';
+import LikeScreen from '../screens/LikeScreen';
+import PostScreen from '../screens/PostScreen';
+import { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
+import { RouteProp } from '@react-navigation/native';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
+
+type TabBarIconProps = {
+    routeName: string;
+    focused: boolean;
+    size: number;
+    color: string;
+};
+
+const getTabBarIcon = ({ routeName, focused, size, color }: TabBarIconProps) => {
+    let iconName: string;
+
+    switch (routeName) {
+        case 'Home':
+            iconName = focused ? 'home' : 'home-outline';
+            break;
+        case 'Search':
+            iconName = focused ? 'search' : 'search-outline';
+            break;
+        case 'Post':
+            iconName = focused ? 'add-circle' : 'add-circle-outline';
+            break;
+        case 'Like':
+            iconName = focused ? 'heart' : 'heart-outline';
+            break;
+        case 'Profile':
+            iconName = focused ? 'person' : 'person-outline';
+            break;
+        default:
+            iconName = 'ellipse-outline';
+            break;
+    }
+
+    return <Icon name={iconName} size={size} color={color} />;
+};
 
 const BottomNavigator = () => {
     return (
         <Tab.Navigator
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ color, focused, size }) => {
-                    let iconName;
-
-                    switch (route.name) {
-                        case 'Home':
-                            iconName = focused ? "home" : 'home-outline';
-                            break;
-                        // case 'Analytics':
-                        //     iconName = focused ? 'analytics' : 'analytics-outline';
-                        //     break;
-                        // case 'Profile':
-                        //     iconName = focused ? 'person' : 'person-outline';
-                        //     break;
-                        // case 'Workout':
-                        //     iconName = focused ? 'barbell' : 'barbell-outline';
-                        //     break;
-                        // case 'Food':
-                        //     iconName = focused ? 'fast-food' : 'fast-food-outline';
-                        //     break;
-                        default:
-                            iconName = 'ellipse-outline';
-                            break;
-                    }
-
-                    return <Icon name={iconName} size={size || 25} color={color} />;
-                },
-                tabBarActiveTintColor: 'tomato',
+            screenOptions={({route}: 
+                { route: RouteProp<Record<string, object | undefined>, string>;}): BottomTabNavigationOptions => ({
+                tabBarIcon: ({ color, focused, size }) =>
+                    getTabBarIcon({ routeName: route.name, focused, size, color }),
+                tabBarActiveTintColor: 'black',
                 tabBarInactiveTintColor: 'gray',
                 tabBarStyle: {
                     display: 'flex',
@@ -49,34 +66,34 @@ const BottomNavigator = () => {
                     headerShown: false,
                 }}
             />
-            {/* <Tab.Screen
-                name="Analytics"
-                component={Analytics}
+            <Tab.Screen
+                name="Search"
+                component={SearchScreen}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Tab.Screen
+                name="Post"
+                component={PostScreen}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Tab.Screen
+                name="Like"
+                component={LikeScreen}
                 options={{
                     headerShown: false,
                 }}
             />
             <Tab.Screen
                 name="Profile"
-                component={Profile}
+                component={ProfileScreen}
                 options={{
                     headerShown: false,
                 }}
             />
-            <Tab.Screen
-                name="Workout"
-                component={Workout}
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Tab.Screen
-                name="Food"
-                component={Food}
-                options={{
-                    headerShown: false,
-                }}
-            /> */}
         </Tab.Navigator>
     );
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 
 const HomeScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Home Screen</Text>
-        </View>
-    );
-}
+  // Create an array with 100 elements
+  const items = Array.from({length: 100}, (_, i) => `Home Screen ${i + 1}`);
+
+  return (
+    <>
+      <View style={styles.tab}>
+        <Text style={styles.tabText}>SocialApp</Text>
+      </View>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <ScrollView style={styles.content}>
+          {items.map((item, index) => (
+            <Text key={index} style={styles.contentText}>
+              {item}
+            </Text>
+          ))}
+        </ScrollView>
+      </ScrollView>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#E5E4E2',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  scrollContainer: {
+    flexGrow: 1,
+  },
+  tab: {
+    backgroundColor: 'black',
+    padding: 10,
+  },
+  tabText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'gray',
+  },
+  content: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: 'white',
+  },
+  contentText: {
+    fontSize: 16,
+    color: 'black',
+    marginBottom: 10, // Optional: Adds space between text items
+  },
 });
 
 export default HomeScreen;
